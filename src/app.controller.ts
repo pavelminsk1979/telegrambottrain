b101faf1de695fa85d7b085430ac4fea22588fd0
pavvelpotapov <pavelminsk1979@mail.ru>
2025-06-15 02:32:19 +0300

    get message with application

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,14 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('ngrok')
+  getNgrok(@Body() payload: any) {
+    console.log('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$');
+    console.log(payload);
+    console.log('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$');
+
+    return { status: '200' };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import axios from 'axios';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3311);
+  const token = '7629205094:AAEaOXVHit2Oq3yzJh3J_rDZa-Q-VQI_fBg';
+  const urlNgrok = 'https://0eea-89-187-171-229.ngrok-free.app/ngrok';
+  await axios.post(`https://api.telegram.org/bot${token}/setWebhook`, {
+    url: urlNgrok,
+  });
+  console.log('ok');
 }
+
 bootstrap();
